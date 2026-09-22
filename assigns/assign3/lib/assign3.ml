372b22ff15d6d1fc88f19fcd0c36480a9b758fb6
Griffin Scafidi-McGuire <gscafmac@bu.edu>
2026-09-22 19:57:19 -0400

    made remove_key tail recursive, finished split_w

--- assigns/assign3/lib/assign3.ml
+++ assigns/assign3/lib/assign3.ml
@@ -1,17 +1,40 @@
-let rec remove_key (k: 'a) (l : ('a * 'b) list) =
+let rec remove_key (k: 'a) (l : ('a * 'b) list) (acc: ('a * 'b) list)=
     match l with
-    | [] -> []
-    | (a, _) :: t when a = k -> remove_key k t
-    | (a, b) :: t -> (a, b) :: remove_key k t
+    | [] -> List.rev acc
+    | (a, _) :: t when a = k -> remove_key k t acc
+    | (a, b) :: t -> remove_key k t ((a,b)::acc)
 
 let rec nub (l : ('a * 'b) list) : ('a * 'b) list =
   match l with
   | [] -> []
-  | (a, b) :: t -> (a, b) :: nub (remove_key a t)
+  | (a, b) :: t -> (a, b) :: nub (remove_key a t [])
 
 
-(*
+let explode (s : string) : char list =
+  let rec loop acc i =
+    if i = String.length s
+    then acc
+    else loop (s.[i] :: acc) (i + 1)
+  in List.rev (loop [] 0)
+
+let implode (l : char list) : string =
+  String.init (List.length l) (List.nth l)
+
 let split_by_ws' (s : string) : string list =
-  let _ = s in
-  assert false
-*)
+let white = function
+  | ' ' | '\n' | '\r' | '\t' -> true
+  | _ -> false
+  in
+  let rec loop (char_list: char list) (acc : char list) (str_list: string list)=
+  if acc = [] then
+    match char_list with
+    | [] -> List.rev str_list
+    | c :: t when white c -> loop t [] str_list
+    | c :: t -> loop t (c::acc) str_list 
+else
+  match char_list with
+  | [] -> List.rev ((implode (List.rev acc)) :: str_list)
+  | c :: t when white c -> loop t [] ((implode (List.rev acc)) :: str_list)
+  | c :: t -> loop t (c::acc) str_list
+in
+loop (explode s) [] []
