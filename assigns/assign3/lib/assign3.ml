73ceb43be093c77cc3f4c2fb63239b1d42a36ed7
Griffin Scafidi-McGuire <gscafmac@bu.edu>
2026-09-22 18:51:00 -0400

    nub finished

--- assigns/assign3/lib/assign3.ml
+++ assigns/assign3/lib/assign3.ml
@@ -1,8 +1,17 @@
+let rec remove_key (k: 'a) (l : ('a * 'b) list) =
+    match l with
+    | [] -> []
+    | (a, _) :: t when a = k -> remove_key k t
+    | (a, b) :: t -> (a, b) :: remove_key k t
 
-let nub (l : ('a * 'b) list) : ('a * 'b) list =
-  let _ = l in
-  assert false
+let rec nub (l : ('a * 'b) list) : ('a * 'b) list =
+  match l with
+  | [] -> []
+  | (a, b) :: t -> (a, b) :: nub (remove_key a t)
 
+
+(*
 let split_by_ws' (s : string) : string list =
   let _ = s in
   assert false
+*)
